6c21ce2ef1db82d9cbc56fd24b3d2c6884199e94
Joel <joelvictor1746@gmail.com>
2022-05-30 22:18:37 -0300

    fix: working on it

--- app/chat_interface.py
+++ app/chat_interface.py
@@ -1,5 +1,7 @@
+import random
 import tkinter as tk
 from functools import partial
+from tkinter import ttk
 
 import Pyro4
 
@@ -26,8 +28,30 @@ class Interface:
             "label": None,
             "button": None,
             "input": None
+        },
+        "select": {
+            "value": tk.StringVar(),
+            "message": "Selecione a sala",
+            "label": None,
+            "button": None,
+            "input": None
+        },
+        "people": {
+            "value": tk.StringVar(),
+            "message": "Participantes",
+            "label": None,
+            "button": None,
+            "input": None
+        },
+        "room": {
+            "value": tk.StringVar(),
+            "message": "Criar sala",
+            "label": None,
+            "button": None,
+            "input": None
         }
     }
+    popup = None
 
     OPTIONS_ROW = 5
     TEXT_COL = 10
@@ -41,6 +65,8 @@ class Interface:
     row = 0
 
     text_width=50
+    chatrooms = []
+
 
     def create_message(self, messages):
         final_txt = ""
@@ -61,17 +87,16 @@ class Interface:
     def create_input(self):
         # input
         self.input["input"]["input"] = tk.Entry(self.master, width=self.text_width)
-        self.input["input"]["input"].grid(row=self.row, column=1, columnspan=3)
+        self.input["input"]["input"].grid(row=self.row, column=0, columnspan=3)
         # button
-        # self.row+=1
         self.input["input"]["button"] = tk.Button(self.master, text="Enviar", command=self.set_input_text, bd=3)
-        self.input["input"]["button"].grid(row=self.row, column=5, columnspan=1)
-
-        # label_test
-        self.row+=1
-        self.label_test = tk.Label(self.master, textvariable=self.input["input"]["value"])
-        self.label_test.config(font=("helvetica", 10))
-        self.label_test.grid(row=self.row, column=0, columnspan=3)
+        self.input["input"]["button"].grid(row=self.row, column=3, columnspan=1)
+        self.row +=1
+        # label
+        self.input["input"]["label"] = tk.Label(self.master, text="Enviando para")
+        self.input["input"]["label"].config(font=("helvetica", 10))
+        self.input["input"]["label"].grid(row=self.row, column=0, columnspan=3)
+        self.row +=1
 
     def create_chat(self):
         # label
@@ -79,22 +104,81 @@ class Interface:
         self.input["chat"]["label"].config(font=("helvetica", 10))
         self.input["chat"]["label"].grid(row=self.row, column=0, columnspan=3)
         # chat
-
         self.row+=1
-        self.input["chat"]["input"] = tk.Text(self.master, state="disabled", width=self.text_width)
+        self.input["chat"]["input"] = tk.Text(self.master, state="normal", width=self.text_width)
         scrollbar = tk.Scrollbar(self.input["chat"]["input"])
         scrollbar.place(relheight=1, relx=0.974)
 
-        # self.input["chat"]["input"] = tk.Entry(self.master, state="disabled")
-        # self.input["chat"]["input"] = tk.Text(self.master,state="disabled")
         self.input["chat"]["input"].grid(row=self.row, column=0, columnspan=3)
         self.row+=1
+
+    def create_dropdown(self):
+        # label
+        self.input["select"]["label"] = tk.Label(self.master, text=self.input["select"]["message"])
+        self.input["select"]["label"].config(font=("helvetica", 10))
+        self.input["select"]["label"].grid(row=self.row, column=0, columnspan=1)
+
+        # select
+        self.input["select"]["value"].set("nenhuma")
+        self.input["select"]["input"] = tk.OptionMenu(self.master, self.input["select"]["value"],"nenhuma", *self.chatrooms)
+        self.input["select"]["input"].grid(row=self.row, column=1, columnspan=1)
+
+        # button
+        self.input["select"]["button"] = tk.Button(self.master, text="Atualizar lista", command=self.update_chatrooms, bd=3)
+        self.input["select"]["button"].grid(row=self.row, column=2, columnspan=1)
+        self.row +=1
+
+        self.input["select"]["button"] = tk.Button(self.master, text="popup", command=self.popup_bonus, bd=3)
+        self.input["select"]["button"].grid(row=self.row, column=2, columnspan=1)
+        self.row +=1
+
+    def update_chatrooms(self):
+        self.chatrooms = [random.randint(0,12) for _ in range(0, random.randint(3,5))]
+
+        menu = self.input["select"]["input"]["menu"]
+        variable = self.input["select"]["value"]
+        menu.delete(0, "end")
+        variable.set("nenhuma")
+
+        menu.add_command(label="nenhuma",
+                         command=lambda value="nenhuma": variable.set(value))
+
+        for string in self.chatrooms:
+            menu.add_command(label=string,
+                             command=lambda value=string: variable.set(value))
+
+
+
+    def popup_bonus(self):
+
+        if self.popup:
+            return
+
+        self.popup = tk.Toplevel()
+        self.popup.wm_title("Window")
+
+        self.input["room"]["label"] = tk.Label(self.popup, text=self.input["room"]["message"])
+        self.input["room"]["label"].config(font=("helvetica", 10))
+        self.input["room"]["label"].grid(row=0, column=0, columnspan=2)
+
+        # input
+        self.input["room"]["input"] = tk.Entry(self.popup, width=self.text_width)
+        self.input["room"]["input"].grid(row=1, column=0, columnspan=3)
+
+        b = ttk.Button(self.popup, text="Okay", command=self.create_room)
+        b.grid(row=2, column=0)
+
+    def create_room(self):
+        print(f"criadno nova sala {self.input['room']['input'].get()}")
+        self.popup.destroy()
+        self.popup = None
 
 
     def start(self):
         self.create_input()
         self.create_chat()
         self.create_people()
+        self.create_dropdown()
         # do loop here
         while True:
             self.master.update()
@@ -103,8 +187,13 @@ class Interface:
         pass
 
     def create_people(self):
+        self.input["people"]["label"] = tk.Label(self.master, text=self.input["people"]["message"])
+        self.input["people"]["label"].config(font=("helvetica", 10))
+        self.input["people"]["label"].grid(row=self.row, column=0, columnspan=1)
+        self.row +=1
+
         choose = []
-        col = 1
+        col = 0
 
         for value in ["joao", "jose", "jose maria da silva souza","joao", "jose","joao", "jose","joao", "jose","joao", "jose","joao", "jose","joao", "jose","joao", "jose","joao", "jose","joao", "jose","joao", "jose",]:
             var = tk.BooleanVar()
@@ -118,9 +207,11 @@ class Interface:
                 command=self.select_choice,
             )
 
-            if col == 7:
+            if col == 3:
                 self.row += 1
-                col = 1
+                col = 0
 
             c.grid(row=self.row, column=col, columnspan=1)
             col += 1
+
+        self.row +=1
